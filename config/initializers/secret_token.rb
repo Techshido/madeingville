5639d3571e7f660712115a0ae2d5c9577f569fbe
Amir Rubin <Amir.Rubin@acceleratedperception.com>
2012-03-24 11:02:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madeingville::Application.config.secret_token = '16bbde551c49c9bbdaa0d84fcc4f5a69ab16dfe021dbb169012a8d052d6d84420d863320a4afb25beed8f937608c81d4c49afc24c92b1541397e89e2c1f8f9ad'
