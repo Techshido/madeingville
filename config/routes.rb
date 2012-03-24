6686463c2ad23245a83880c2696af6b73b98e7d3
Amir Rubin <Amir.Rubin@acceleratedperception.com>
2012-03-24 17:36:43 -0400

    Adding user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Madeingville::Application.routes.draw do
   
   devise_for :users
+  resources :users
 
   root to: 'pages#home'
   
@@ -8,57 +9,30 @@ Madeingville::Application.routes.draw do
   match 'biz_monkeys' => 'pages#biz_monkey_list'
   match 'hackers' => 'pages#hacker_list'
   
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
+#== Route Map
+# Generated on 24 Mar 2012 17:29
+#
+#             user_session POST   /users/sign_in(.:format)       devise/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
+#            user_password POST   /users/password(.:format)      devise/passwords#create
+#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+#                          PUT    /users/password(.:format)      devise/passwords#update
+# cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
+#        user_registration POST   /users(.:format)               devise/registrations#create
+#    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
+#                          PUT    /users(.:format)               devise/registrations#update
+#                          DELETE /users(.:format)               devise/registrations#destroy
+#                    users GET    /users(.:format)               users#index
+#                          POST   /users(.:format)               users#create
+#                 new_user GET    /users/new(.:format)           users#new
+#                edit_user GET    /users/:id/edit(.:format)      users#edit
+#                     user GET    /users/:id(.:format)           users#show
+#                          PUT    /users/:id(.:format)           users#update
+#                          DELETE /users/:id(.:format)           users#destroy
+#                     root        /                              pages#home
+#                    about        /about(.:format)               pages#about
+#              biz_monkeys        /biz_monkeys(.:format)         pages#biz_monkey_list
+#                  hackers        /hackers(.:format)             pages#hacker_list
