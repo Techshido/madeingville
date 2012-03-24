1244159dbda75ed185585e9640a7dba6685e570c
Preston Mueller <preston.mueller@techshido.com>
2012-03-24 16:33:02 -0400

    fixes for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,33 +15,33 @@
 # looking_for (Technology), projects (Project)
 
 
-passwd = "foobar"
-email_suffix = "@test.com"
+@passwd = "foobar"
+@email_suffix = "@test.com"
 
-technologies = %w[web_dev ios android]
-achievements = %w[internship open_source real_world contest published_paper class]
-compensations = %w[cash experience equity]
+@technologies = %w[web_dev ios android]
+@achievements = %w[internship open_source real_world contest published_paper class]
+@compensations = %w[cash experience equity]
 
-first_names = %w[paul ringo john george bob mike joe chris]
-last_names = %w[smith jones]
+@first_names = %w[paul ringo john george bob mike joe chris]
+@last_names = %w[smith jones]
 
 # Make the hackers
 def make_hackers
   current = 1
   100.times do 
 
-    email = "hacker#{current}#{email_suffix}"
+    email = "hacker#{current}#{@email_suffix}"
 
     User.find_or_initialize_by_email(email).tap do |t|
-      t.password = passwd
-      t.password_confirmation = passwd
+      t.password = @passwd
+      t.password_confirmation = @passwd
       t.role = :hacker
-      t.first_name = first_names.shuffle.first
-      t.last_name = last_names.shuffle.first
+      t.first_name = @first_names.shuffle.first
+      t.last_name = @last_names.shuffle.first
 
-      t.interests = [Technology.new(user_id: t.id, type: technologies.shuffle.first)]
-      t.achievements = [Achievement.new(hacker_id: t.id, type: achievements.shuffle.first])
-      t.will_work_for = Compensation.new(hacker_id: t.id, type: compensations.shuffle.first
+      t.interests = [Technology.new(user_id: t.id, type: @technologies.shuffle.first)]
+      t.achievements = [Achievement.new(hacker_id: t.id, type: @achievements.shuffle.first)]
+      t.will_work_for = Compensation.new(hacker_id: t.id, type: @compensations.shuffle.first)
     end
 
     current = current + 1
@@ -53,16 +53,16 @@ end
 def make_biz_monkies
   current = 1
   30.times do
-    email = "bizmonkey#{current}#{email_suffix}"
+    email = "bizmonkey#{current}#{@email_suffix}"
     User.find_or_initialize_by_email(email).tap do |t|
-      t.password = passwd
-      t.password_confirmation = passwd
+      t.password = @passwd
+      t.password_confirmation = @passwd
       t.role = "biz_monkey"
-      t.first_name = first_names.shuffle.first
-      t.last_name = last_names.shuffle.first
+      t.first_name = @first_names.shuffle.first
+      t.last_name = @last_names.shuffle.first
 
-      t.looking_for = [Technology.new(user_id: t.id, type: technologies.shuffle.first)]
-      t.projects = [Project.new(biz_monkey_id: t.id, description: "Description of an awesome test project)]
+      t.looking_for = [Technology.new(user_id: t.id, type: @technologies.shuffle.first)]
+      t.projects = [Project.new(biz_monkey_id: t.id, description: "Description of an awesome test project")]
     end
   end
   current = current + 1
@@ -72,3 +72,4 @@ namespace :db do
   User.destroy_all
   make_hackers
   make_biz_monkies
+end
